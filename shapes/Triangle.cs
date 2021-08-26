3822366d93ccfdf40603d1098f38d61b7078d1d7
itays123 <itays2005@gmail.com>
2021-08-26 22:01:23 +0300

    FEATURE: Draw triangle

--- shapes/Triangle.cs
+++ shapes/Triangle.cs
@@ -4,6 +4,12 @@ namespace Shapes
 {
     public class Triangle : Shape
     {
+
+        // draw refrences
+        private static string TRIANGLE_UNIT = "#";
+        private static string SPACE = " ";
+
+        // attributes and properties
         private int _base;
         public int Base { 
             get { return _base; } 
@@ -23,9 +29,31 @@ namespace Shapes
 
         public override string Draw()
         {
-            return "";
-        }
+            string s = "";
+            int margin;
+            for (int y = Height; y > 0; --y)
+            {
+                s+=TRIANGLE_UNIT;
+                // because the longest edge is at the line y=(-h/b)x + h (between [0,h] and [b,0])
+                // the margin (x), represented by spaces, would equal b-by/h. 
+                // One margin is represented by the starting triangle unit, so subtract one from the result.
+                margin = Base  - Base * y / Height - 1;
+                if (margin >= 0) {
+                    for (int i = 0; i < margin; i++)
+                        s+=SPACE;
+                    s+=TRIANGLE_UNIT;
+                }
+                    
+                s+="\n";
+            }
 
+            // add base
+            for (int i = 0; i < Base; i++)
+            {
+                s+=TRIANGLE_UNIT;
+            }
+            return s;
+        }
         public override double GetArea()
         {
             return _base * _height / 2;
